d3b7fd87fe5c76c1d3d92479aa932fca203fbbcf
Merak1 <arias_j@outlook.com>
2018-06-30 17:26:16 -0500

    añadido slider
    añadido slider

--- buenosDiasMandy.js
+++ buenosDiasMandy.js
@@ -1,24 +1,24 @@
 
-		today = new Date()
-		if(today.getMinutes() < 10){
-		pad = "0"}
-		else
-		pad = "";
-		document.write ;if((today.getHours() >=6) && (today.getHours() <=9)){
-		document.getElementById("mandy").innerHTML ="¡Buenos Días!";
-		}
-		if((today.getHours() >=10) && (today.getHours() <=11)){
-		document.getElementById("mandy").innerHTML ="¡Buenos Días!";
-		}
-		if((today.getHours() >=12) && (today.getHours() <=19)){
-		document.getElementById("mandy").innerHTML ="¡Buenas Tardes!";
-		}
-		if((today.getHours() >=20) && (today.getHours() <=23)){
-		document.getElementById("mandy").innerHTML ="¡Buenas Noches!";
-		}
-		if((today.getHours() >=0) && (today.getHours() <=3)){
-		document.getElementById("mandy").innerHTML ="¡Buenas Noches!";
-		}
-		if((today.getHours() >=4) && (today.getHours() <=5)){
-		document.getElementById("mandy").innerHTML ="¡Buenos Días!";
-		}
+		// today = new Date()
+		// if(today.getMinutes() < 10){
+		// pad = "0"}
+		// else
+		// pad = "";
+		// document.write ;if((today.getHours() >=6) && (today.getHours() <=9)){
+		// document.getElementById("mandy").innerHTML ="¡Buenos Días!";
+		// }
+		// if((today.getHours() >=10) && (today.getHours() <=11)){
+		// document.getElementById("mandy").innerHTML ="¡Buenos Días!";
+		// }
+		// if((today.getHours() >=12) && (today.getHours() <=19)){
+		// document.getElementById("mandy").innerHTML ="¡Buenas Tardes!";
+		// }
+		// if((today.getHours() >=20) && (today.getHours() <=23)){
+		// document.getElementById("mandy").innerHTML ="¡Buenas Noches!";
+		// }
+		// if((today.getHours() >=0) && (today.getHours() <=3)){
+		// document.getElementById("mandy").innerHTML ="¡Buenas Noches!";
+		// }
+		// if((today.getHours() >=4) && (today.getHours() <=5)){
+		// document.getElementById("mandy").innerHTML ="¡Buenos Días!";
+		// }
--- slide.js
+++ slide.js
@@ -1,18 +1,23 @@
-var slideIndex = 1;
+var slider_content = document.getElementById('box');
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+var imagen = ["imagen1","imagen2","imagen3"];
+
+var i = imagen.length;
+
+function next(){
+  if (i<imagen.length){
+    i = i+1;
+  }else{
+    i = 1;
+  }
+  slider_content.innerHTML = "<img src="+imagen[i-1]+".png>";
 }
 
-function showDivs(n) {
-    var i;
-    var slideIndex = 1;
-    var x = document.getElementsByClassName("slides");
-    if (n > x.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    x[slideIndex-1].style.display = "block";  
+function last(){
+  if (i<imagen.length +1 && i>1 ){
+    i = i-1;
+  }else{
+    i = imagen.length;
+  }
+  slider_content.innerHTML = "<img src="+imagen[i-1]+".png>";
 }
-showDivs(slideIndex);
